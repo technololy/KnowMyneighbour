a65c5dddd9288ab35251aad7b99b777c2cb1d01e
Ololade Oyebanji <loladeking@hotmail.com>
2017-09-12 13:14:15 +0100

    Add project files.

---
+++ KnowMyneighbour/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KnowMyneighbour.Startup))]
+namespace KnowMyneighbour
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
